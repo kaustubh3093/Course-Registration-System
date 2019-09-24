8b2a666f710dd9266d15bbf1e38e8bf662fdcdfd
kaustubh3093 <kaustubhn.singh@gmail.com>
2019-09-23 20:55:08 -0400

    Interface

---
+++ coursesRegistration/src/coursesRegistration/util/FileDisplayInterface.java
@@ -0,0 +1,7 @@
+package coursesRegistration.util;
+
+public interface FileDisplayInterface {
+	public void coursedata(String args);
+	public void studentdata(String args);
+	public void putoutput(String args);
+}
